89e835dd9740be203694b2008af98d594eec79c9
leilei <248721866@qq.com>
2020-08-17 22:12:45 +0800

    【新增】 consul 客户端demo

---
+++ springcloud-consul-demo/src/main/java/com/leilei/SpringcloudConsulDemoApplication.java
@@ -0,0 +1,15 @@
+package com.leilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringcloudConsulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConsulDemoApplication.class, args);
+    }
+
+}
