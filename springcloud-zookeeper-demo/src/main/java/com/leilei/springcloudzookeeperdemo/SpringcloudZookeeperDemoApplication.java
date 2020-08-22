fd9fceac8f4733d172e3ef4a14bf0ef25c0e7a51
leilei <248721866@qq.com>
2020-08-22 17:09:45 +0800

    zookeeper 客户端简单搭建

---
+++ springcloud-zookeeper-demo/src/main/java/com/leilei/springcloudzookeeperdemo/SpringcloudZookeeperDemoApplication.java
@@ -0,0 +1,15 @@
+package com.leilei.springcloudzookeeperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringcloudZookeeperDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudZookeeperDemoApplication.class, args);
+    }
+
+}
---
+++ springcloud-zookeeper-demo/src/test/java/com/leilei/springcloudzookeeperdemo/SpringcloudZookeeperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leilei.springcloudzookeeperdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudZookeeperDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
