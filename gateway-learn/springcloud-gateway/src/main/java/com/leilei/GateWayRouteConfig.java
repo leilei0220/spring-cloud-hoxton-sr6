b959d25f8db9482ba7e36c776ebc4b35c6ad5706
leilei <5176939+leileispace@user.noreply.gitee.com>
2020-10-12 22:13:38 +0800

    【新增】 gateway组件的使用

---
+++ gateway-learn/springcloud-gateway/src/main/java/com/leilei/GateWayRouteConfig.java
@@ -0,0 +1,25 @@
+//package com.leilei;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author lei
+// * @version 1.0
+// * @date 2020/10/12 21:25
+// * @desc gateway网关java配置路由信息
+// */
+//@Configuration
+//public class GateWayRouteConfig {
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("easy-order", r -> r.path("/order/**")
+//                        .uri("http://localhost:9002/"))
+//                .route("server-product", r -> r.path("/product/**")
+//                        .uri("lb://demo-product"))
+//                .build();
+//    }
+//}
---
+++ gateway-learn/springcloud-gateway/src/main/java/com/leilei/SpringcloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.leilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient //consul客户端此注解可省略 ，eureka客户端的话则必需
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+
+}
