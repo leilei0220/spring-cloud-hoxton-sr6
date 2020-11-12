f4b2d60f1e3904de1e05c23227aba6dd5dbf2bd3
leilei <5176939+leileispace@user.noreply.gitee.com>
2020-11-12 21:28:06 +0800

    【新增】 手动刷新服务配置

--- open-feign-hystrix-demo-learn/springcloud-openfeign-hystrix-order/src/main/java/com/leilei/demo/OrderController.java
+++ open-feign-hystrix-demo-learn/springcloud-openfeign-hystrix-order/src/main/java/com/leilei/demo/OrderController.java
@@ -2,6 +2,7 @@ package com.leilei.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
  * @desc
  */
 @RestController
+@RefreshScope
 @RequestMapping("/order")
 public class OrderController {
     @Value("${student.name}")
